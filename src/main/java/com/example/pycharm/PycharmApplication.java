4a813ded63d08655a3705cbafa1c95638d339c31
Rajasekhar Seshasaye Velamuri <rajasekhar.velamuri@tavant.com>
2020-02-12 22:52:56 +0530

    First commit

---
+++ src/main/java/com/example/pycharm/PycharmApplication.java
@@ -0,0 +1,13 @@
+package com.example.pycharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PycharmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PycharmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pycharm/PycharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pycharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PycharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
